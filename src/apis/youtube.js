be15e8d5a76d1c7d980fd88281b625a83a9321da
Henry Richardson <hrichardson83@gmail.com>
2019-03-25 16:43:31 -0400

    searching for videos

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyC0dTFzQQZ6IdzUYBE2gCjBzntdsOLdVTg';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+})
