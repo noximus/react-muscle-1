2771d7f034d4c941628274b16de05a1427666395
Henry Richardson <hrichardson83@gmail.com>
2019-03-25 16:15:12 -0400

    handling form submittal

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
- class SearchBar extends Component {
+class SearchBar extends Component {
+  state = { term: '' };
+
+  onInputChange = evt => {
+    this.setState({ term: evt.target.value });
+  }
+
+  onFormSubmit = evt => {
+    evt.preventDefault();
+    // this.setState({ term: evt.target.value });
+  }
+  
+
   render() {
     return (
-      <div>
-        SearchBar
+      <div className="search-bar ui segment">
+        <form action="" className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label htmlFor="">Video Search</label>
+            <input 
+              type="text"
+              name=""
+              id=""
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
       </div>
     )
   }
